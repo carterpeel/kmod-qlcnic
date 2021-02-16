11353f9b1a0ec098da89b86557be3ca174d466da
Carter Peel <carterpeel@gmail.com>
2021-02-16 12:05:15 -0500

    Lil' optimization

--- auto-QLCNic-el8.sh
+++ auto-QLCNic-el8.sh
@@ -8,9 +8,9 @@ qlnicdrivers () {
     mkdir qlnic
     cd qlnic/
     
-    wget https://github.com/liamalxd/kmod-qlcnic/blob/main/qlcnic_el8_3.ko?raw=true
+    wget https://github.com/liamalxd/kmod-qlcnic/blob/main/qlcnic_el8_3.ko
     mkdir -p /lib/modules/4.18.0-240.el8.x86_64/extra/qlcnic/
-    mv 'qlcnic_el8_3.ko?raw=true' qlcnic.ko
+    mv 'qlcnic_el8_3.ko' qlcnic.ko
     cp qlcnic.ko /lib/modules/4.18.0-240.el8.x86_64/extra/qlcnic/
     echo "install qlcnic insmod /lib/modules/4.18.0-240.el8.x86_64/extra/qlcnic/qlcnic.ko" > /etc/modprobe.d/qlcnic.conf
     echo "qlcnic" > /etc/modules-load.d/qlcnic.conf
